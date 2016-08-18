8d4395004687289db413481fb62a139360ab3c97
im-mi <im.mi.mail.mi@gmail.com>
2016-08-17 23:16:07 -0400

    Unminified autocomplete code

--- theme.php
+++ theme.php
@@ -5,7 +5,7 @@ class LegacyAutoCompleteTheme extends Themelet {
 		$base_href = get_base_href();
 		// TODO: AJAX test and fallback.
 
-		$page->add_html_header("<script src='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete-2.4.4.min.js' type='text/javascript'></script>");
-		$page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete-2.4.4.min.css' />");
+		$page->add_html_header("<script src='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete.js' type='text/javascript'></script>");
+		$page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete.css' />");
 	}
 }
