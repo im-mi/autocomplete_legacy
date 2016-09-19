af9e5e69db50ec7ee874d53bd871dac6b605d0a2
im-mi <im.mi.mail.mi@gmail.com>
2016-09-19 01:07:35 -0400

    Redid search query normalization + fixed / re-enabled caching

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 $(function(){
-	var isNegative = false;
-	
+	var normalizeSearchQuery = function(s) {
+		s = s ? s : "";
+		s = s.trim();
+		s = s && s[0] === '-' ? s.substr(1) : s;
+		return s;
+	}
+
 	$('[name=search],.autocomplete_tags').autocomplete(base_href + '/api/internal/autocomplete_legacy', {
 		//extraParams: {limit: 10},
 		queryParamName: 's',
 		minChars: 1,
 		delay: 0,
-		useCache: false,
+		useCache: true,
 		maxCacheLength: 10,
 		matchInside: false,
 		selectFirst: true,
@@ -24,19 +29,41 @@ $(function(){
 			$('<span></span>', { text: data, class:"acTagCount" }).appendTo(root);
 			return root.html();
 		},
-		beforeUseConverter: function(s) {
-			isNegative = s[0] === '-';
-			return isNegative ? s.substr(1) : s;
-		},
-		processData: function(results) {
-			if (isNegative) {
-				for (var i = 0; i < results.length; i++)
-					results[i].value = "-" + results[i].value;
+	}).each(function() {
+		var ac = $(this).data("autocompleter");
+
+		var searchQuery;
+
+		ac.options.beforeUseConverter = function(s) {
+			//
+			// Here search queries are normalized before being submitted to the server.
+			// Additionally, the original search query is recorded for later use.
+			//
+			searchQuery = s;
+			return normalizeSearchQuery(s);
+		};
+
+		ac.options.filter = function(result, filter) {
+			if (!result.value) return false;
+			if (!ac.options.filterResults) return true;
+
+			var pattern = normalizeSearchQuery(filter);
+			var testValue = normalizeSearchQuery(result.value);
+			if (!ac.options.matchCase) {
+				pattern = pattern.toLowerCase();
+				testValue = testValue.toLowerCase();
 			}
-			return results;
-		},
-		matchStringConverter: function(s, a, b) {
-			return s[0] === '-' ? s.substr(1) : s;
-		}
+			var patternIndex = testValue.indexOf(pattern);
+
+			// Adjust the result to match the sign of the original search query.
+			if (searchQuery && searchQuery.trim()[0] === '-')
+				result.value = "-" + result.value;
+
+			if (ac.options.matchInside) {
+				return patternIndex > -1;
+			} else {
+				return patternIndex === 0;
+			}
+		};
 	});
 });
