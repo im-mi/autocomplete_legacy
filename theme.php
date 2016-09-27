fc6f2f01e1e6c21cb3dd92099133b5b85b43ea4c
im-mi <im.mi.mail.mi@gmail.com>
2016-09-27 01:32:56 -0400

    Formatting

--- theme.php
+++ theme.php
@@ -12,6 +12,6 @@ class LegacyAutoCompleteTheme extends Themelet {
 			$page->add_html_header("<meta name='autocomplete_legacy_select_first' content=''>");
 
 		$page->add_html_header("<script src='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete.js' type='text/javascript'></script>");
-		$page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete.css' />");
+		$page->add_html_header("<link rel='stylesheet' type='text/css' href='$base_href/ext/autocomplete_legacy/lib/jquery.autocomplete.css'/>");
 	}
 }
